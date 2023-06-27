ba924b3a1a15e6c8a9cd10c577585ff3eea70f8c
Rishabh <rigu497@gmail.com>
2023-06-27 13:15:54 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import News from './components/News';
+import Navbar from './components/navbar';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Navbar></Navbar>
+        <div >
+        <Routes>
+        <Route exact path="/" element={<News key="general" category='general'></News>}/>
+        <Route exact path="/business" element={<News  key="business" category='business'></News>}/>
+        <Route exact path="/entertainment" element={<News key="entertainment" category='entertainment'></News>}/>
+        <Route exact path="/health" element={<News key="health" category='health'></News>}/>
+        <Route exact path="/sports" element={<News key="sports" category='sports'></News>}/>
+        <Route exact path="/technology" element={<News  key="technology" category='technology'></News>}/>
+        <Route exact path="/science" element={<News key="science" category='science'></News>}/>
+        </Routes>
+      </div>
+      </Router>
+      
+    )
+  }
 }
 
-export default App;
---
+++ src/components/News.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+import InfiniteScroll from "react-infinite-scroll-component";
+
+export default class News extends Component {
+    constructor(props){
+        super();
+        this.state={
+            article:[],
+            page:1,
+            totalresult:0
+        }
+        // {props.category==='general'?;}
+        if (props.category!='general') {
+          document.title=`News App - ${props.category}`
+        }
+        else{
+          document.title=`News App`
+        }
+        
+        
+    }
+    
+    componentDidMount() {
+      fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=192ca5a526424dfa8014d8ab023cfddd&page=1`)
+      .then((response) => response.json())
+      .then(responsejson => {
+          this.setState({ article: responsejson.articles,totalresult:responsejson.totalResults });
+      });
+    }
+    fetchMoreData = () => {
+      
+      fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=192ca5a526424dfa8014d8ab023cfddd&page=${this.state.page+1}`)
+      .then((response) => response.json())
+      .then(responsejson => {
+          this.setState({ article: this.state.article.concat(responsejson.articles),totalresult:responsejson.totalResults ,page:this.state.page+1});
+      });
+    };
+    
+  render() {
+    return (
+      <div>
+        <h1 className='text-center ' style={{marginTop:"90px"}}>Top Headlines</h1>
+        <InfiniteScroll
+          dataLength={this.state.article.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.article.length!==this.state.totalresult}
+          loader={this.hasmore && <h4 className='text-center'>Loading...</h4>}
+        >
+        <div className='container my-1'>
+        <div className="row">
+        {this.state.article.map((element)=>{
+            return <div className="col-md-4 " key={element.url}>
+            <NewsItem title={element.title} description={element.description?element.description.slice(0,45):""} imageUrl={element.urlToImage} newsUrl={element.url}></NewsItem>
+            </div>
+        })}  
+        </div>
+        </div>
+        
+        </InfiniteScroll>
+      </div>
+    )
+  }
+}
+
+
+
+//   apikey:192ca5a526424dfa8014d8ab023cfddd
